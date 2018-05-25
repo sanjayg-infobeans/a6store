c002cc53a13c8cf5058d61d85c093d577ab4f04d
Sanjay Gajera <sanjay.gajera@infobeans.com>
2018-05-25 23:29:37 +0530

    ui setup

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from "src/app/recipes/recipe-list/recipe.model";
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('First Recipe', 'First Recipe Description', 'http://images.media-allrecipes.com/userphotos/960x960/3758842.jpg'),
+    new Recipe('Second Recipe', 'Second Recipe Description', 'https://cdn3.tmbi.com/secure/RMS/attachments/37/300x300/exps25157_FM153592B03_18_12b.jpg')
+  ];
 
   constructor() { }
 
